ba207596ac61523610ad8a88e51af93dcc57d870
AttigH <hamzaatttig@gmail.com>
2021-12-24 13:12:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,68 +1,110 @@
-import React, {Component} from 'react';
-import './App.css';
-import 'h8k-components';
+import React, { Component } from "react";
+import "./App.css";
+import "h8k-components";
 import ProductList from "./components/product-list";
 import Cart from "./components/cart";
 
 const title = "HackerShop";
 
 class App extends Component {
-    constructor() {
-        super();
-        const products = [...PRODUCTS].map((product, index) => {
-            product.id = index + 1;
-            product.image = `/images/items/${product.name.toLocaleLowerCase()}.png`;
-            product.cartQuantity = 0;
-            return product;
+  constructor() {
+    super();
+    const products = [...PRODUCTS].map((product, index) => {
+      product.id = index + 1;
+      product.image = `/images/items/${product.name.toLocaleLowerCase()}.png`;
+      product.cartQuantity = 0;
+      return product;
+    });
+    this.state = {
+      cart: {
+        items: [],
+      },
+      products,
+    };
+  }
+
+  togglerAddRemove = (product, action) => {
+    if (action === "add") {
+      const items = [...this.state.cart.items];
+      const ExistedItem =
+        items.filter((val) => val.id === product.id).length > 0;
+      if (ExistedItem) {
+        const newItems = items.map((val) => {
+          if (val.id === product.id) {
+            val.cartQuantity = val.cartQuantity + 1;
+          }
+          return val;
         });
-        this.state = {
-            cart: {
-                items: []
-            },
-            products
-        }
+        this.setState({
+          cart: {
+            items: newItems,
+          },
+        });
+      } else {
+        product.cartQuantity += 1;
+        this.state.cart.items.push(product);
+        this.setState({
+          cart: { items: this.state.cart.items },
+        });
+      }
+    } else if (action === "remove") {
+      const items = [...this.state.cart.items];
+      const newItems = items
+        .map((val) => {
+          if (val.id === product.id) {
+            val.cartQuantity = val.cartQuantity - 1;
+          }
+          return val;
+        })
+        .filter((val) => val.cartQuantity > 0);
+      this.setState({
+        cart: {
+          items: newItems,
+        },
+      });
     }
+  };
 
-  
-    
-
-    render() {
-        return (
-            <div>
-                <h8k-navbar header={title}></h8k-navbar>
-                <div className="layout-row shop-component">
-                    <ProductList products={this.state.products}/>
-                    <Cart cart={this.state.cart}/>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h8k-navbar header={title}></h8k-navbar>
+        <div className="layout-row shop-component">
+          <ProductList
+            togglerAddRemove={this.togglerAddRemove}
+            products={this.state.products}
+          />
+          <Cart cart={this.state.cart} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export const PRODUCTS = [
-    {
-        name: "Cap",
-        price: 5
-    },
-    {
-        name: "HandBag",
-        price: 30
-    },
-    {
-        name: "Shirt",
-        price: 35
-    },
-    {
-        name: "Shoe",
-        price: 50
-    },
-    {
-        name: "Pant",
-        price: 35
-    },
-    {
-        name: "Slipper",
-        price: 25
-    }
+  {
+    name: "Cap",
+    price: 5,
+  },
+  {
+    name: "HandBag",
+    price: 30,
+  },
+  {
+    name: "Shirt",
+    price: 35,
+  },
+  {
+    name: "Shoe",
+    price: 50,
+  },
+  {
+    name: "Pant",
+    price: 35,
+  },
+  {
+    name: "Slipper",
+    price: 25,
+  },
 ];
 export default App;
--- src/components/product-list/index.js
+++ src/components/product-list/index.js
@@ -2,62 +2,85 @@ import React, { Component } from "react";
 import "./index.css";
 
 export default class ProductList extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-            <div className="layout-row wrap justify-content-center flex-70 app-product-list">
-                {this.props.products.map((product, i) => {
-                    return (
-                        <section className="w-30"
-                            data-testid={'product-item-' + i}
-                            key={product.id}>
-                            <div className="card ma-16">
-                                <img alt="Your Cart" src={product.image}
-                                    className="d-inline-block align-top product-image" />
-                                <div className="card-text pa-4">
-                                    <h5 className="ma-0 text-center">{product.name}</h5>
-                                    <p className="ma-0 mt-8 text-center">${product.price}</p>
-                                </div>
-                                <div className="card-actions justify-content-center pa-4">
+  handleClick = (product) => {
+    this.props.togglerAddRemove(product, "add");
+  };
+  removeHandleClick = (product) => {
+    this.props.togglerAddRemove(product, "remove");
+  };
+  addHandleClick = (product) => {
+    this.props.togglerAddRemove(product, "add");
+  };
 
-                                    <button className="x-small outlined" data-testid="btn-item-add"
-                                    >
-                                        Add To Cart
-                                    </button>
+  render() {
+    return (
+      <div className="layout-row wrap justify-content-center flex-70 app-product-list">
+        {this.props.products.map((product, i) => {
+          return (
+            <section
+              className="w-30"
+              data-testid={"product-item-" + i}
+              key={product.id}
+            >
+              <div className="card ma-16">
+                <img
+                  alt="Your Cart"
+                  src={product.image}
+                  className="d-inline-block align-top product-image"
+                />
+                <div className="card-text pa-4">
+                  <h5 className="ma-0 text-center">{product.name}</h5>
+                  <p className="ma-0 mt-8 text-center">${product.price}</p>
+                </div>
+                <div className="card-actions justify-content-center pa-4">
+                  <button
+                    className="x-small outlined"
+                    data-testid="btn-item-add"
+                    onClick={() => this.handleClick(product)}
+                  >
+                    Add To Cart
+                  </button>
 
-                                    <div className="layout-row justify-content-between align-items-center">
-                                        <button className="x-small icon-only outlined"
-                                            data-testid="btn-quantity-subtract"
-                                        >
-                                            <i className="material-icons">remove</i>
-                                        </button>
+                  <div className="layout-row justify-content-between align-items-center">
+                    <button
+                      className="x-small icon-only outlined"
+                      data-testid="btn-quantity-subtract"
+                      onClick={() => this.removeHandleClick(product)}
+                    >
+                      <i className="material-icons">remove</i>
+                    </button>
 
-                                        <input type="number"
-                                            disabled
-                                            className="cart-quantity" data-testid="cart-quantity" value={product.cartQuantity} />
+                    <input
+                      type="number"
+                      disabled
+                      className="cart-quantity"
+                      data-testid="cart-quantity"
+                      value={product.cartQuantity}
+                    />
 
-                                        <button className="x-small icon-only outlined"
-                                            data-testid="btn-quantity-add">
-                                            <i className="material-icons">add</i>
-                                        </button>
-                                    </div>
-                                
-                                </div>
-                            </div>
-                        </section>
-                    )
-                })}
-
-            </div>
-
-        );
-    }
+                    <button
+                      className="x-small icon-only outlined"
+                      data-testid="btn-quantity-add"
+                      onClick={() => this.addHandleClick(product)}
+                    >
+                      <i className="material-icons">add</i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </section>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export const UpdateMode = {
-    ADD: 1,
-    SUBTRACT: 0
-}
+  ADD: 1,
+  SUBTRACT: 0,
+};
